f02c67090ef4ae9f773369980db8f38d89cc6003
juanpablocartin <jpcartinvillalobos@outlook.com>
2023-09-13 20:22:16 -0600

    j

---
+++ src/main/java/Vista/VenPri.java
@@ -0,0 +1,58 @@
+package Vista;
+
+import Con.Controladora;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+
+public class VenPri extends javax.swing.JFrame {
+
+    public VenPri() {
+        initComponents();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public JTabbedPane getTABpri() {
+        return TABpri;
+    }
+    
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        TABpri = new javax.swing.JTabbedPane();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(TABpri, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1073, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(TABpri, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 550, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                TipInstruJPanel pti=new TipInstruJPanel();
+                CalibracionesJPanel pcali=new CalibracionesJPanel();
+                InstruJPanel pinstru=new InstruJPanel();
+                VenPri v=new VenPri(); 
+                Controladora c=new Controladora(v,pti,pinstru,pcali);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JTabbedPane TABpri;
+    // End of variables declaration//GEN-END:variables
+}
