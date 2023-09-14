b44c3c22aa4853e09e65d827916416876b6fa899
juanpablocartin <jpcartinvillalobos@outlook.com>
2023-09-13 20:04:51 -0600

    e

--- src/main/java/Principal/Principal.java
+++ src/main/java/Principal/Principal.java
@@ -1,7 +1,7 @@
 package Principal;
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Okay vamos a ver que");
         System.out.println("Hello World!");
 
     }
