aaaef0e7591330f9a30f8929b851858e6d8aa037
juanpablocartin <jpcartinvillalobos@outlook.com>
2023-09-20 17:56:07 -0600

    Vaya Cambios

--- src/main/java/Archivos/ArchivosXML.java
+++ src/main/java/Archivos/ArchivosXML.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Archivos;
+package Archivos;//oooo
 
 import Modelo.Instrumento;
 import Modelo.ListaINSTRUMENTOS;
